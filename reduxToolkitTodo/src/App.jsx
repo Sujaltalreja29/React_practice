46df92ed6f649bba3ca7c15125aa1f6f5aefcbe9
Sujaltalreja29 <sujaltlrj@gmail.com>
2024-12-08 11:37:51 +0530

    Redux-Toolkit

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/Todos'
+import Todos from './components/AddTodo'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>react Redux Toolkit</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
